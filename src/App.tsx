760f0c1630b08c4509ebca54c1411947ac7ce4b7
Nathan Pinciak <nathan.pinciak@outlook.com>
2024-03-30 13:12:48 -0400

    Move `core` directory under `src` add `HeaderCompnent`

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { AppRouter } from './app.routing';
+import { HeaderComponent } from './core';
 
 function App() {
   return (
     <div className="App">
+      <HeaderComponent />
+
       <RouterProvider router={AppRouter} />
     </div>
   );
