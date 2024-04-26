32ef1bb3ac1a9b58111ba3085e892bcb49ba0bc6
Nathan Pinciak <nathan.pinciak@outlook.com>
2024-04-26 15:30:17 -0400

    Add `fangraphs-constants` endpoint

--- src/@shared/supabase/supabase.client.ts
+++ src/@shared/supabase/supabase.client.ts
@@ -1,6 +1,7 @@
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './supabase-database.model';
+import { SupaClientFangraphsConstantsTable } from './supabase-tables.model';
 
 export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 
@@ -16,7 +17,15 @@ export const supabaseClient = createApi({
         return { data };
       },
     }),
+    getFangraphsConstantsBySeason: builder.query<SupaClientFangraphsConstantsTable[], { seasonId: string }>({
+      queryFn: async args => {
+        const { data, error } = await supabase.from('fangraphs-constants').select().eq('season', args.seasonId);
+        if (error) return { error };
+
+        return { data };
+      },
+    }),
   }),
 });
 
-export const { useGetLeagueProgressionQuery } = supabaseClient;
+export const { useGetLeagueProgressionQuery, useGetFangraphsConstantsBySeasonQuery } = supabaseClient;
