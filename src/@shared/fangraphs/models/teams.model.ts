2daa67dc1152a260c30c3b646669068e0864e0e6
Nathan Pinciak <nathan.pinciak@outlook.com>
2024-05-08 08:01:21 -0400

    Add Fangraphs models

---
+++ src/@shared/fangraphs/models/teams.model.ts
@@ -0,0 +1,160 @@
+export const enum FangraphTeam {
+  AllTeams,
+  Angels,
+  Orioles,
+  RedSox,
+  WhiteSox,
+  Guardians,
+  Tigers,
+  Royals,
+  Twins,
+  Yankees,
+  Athletics,
+  Mariners,
+  Rays,
+  Rangers,
+  BlueJays,
+  Diamondbacks,
+  Braves,
+  Cubs,
+  Reds,
+  Rockies,
+  Marlins,
+  Astros,
+  Dodgers,
+  Brewers,
+  Nationals,
+  Mets,
+  Phillies,
+  Pirates,
+  Cardinals,
+  Padres,
+  Giants,
+}
+
+export const FangraphsTeamList = [
+  {
+    value: [FangraphTeam.AllTeams],
+    label: 'All Teams',
+  },
+  {
+    value: [FangraphTeam.Angels],
+    label: 'Angels',
+  },
+  {
+    value: [FangraphTeam.Astros],
+    label: 'Astros',
+  },
+  {
+    value: [FangraphTeam.Athletics],
+    label: 'Athletics',
+  },
+  {
+    value: [FangraphTeam.BlueJays],
+    label: 'Blue Jays',
+  },
+  {
+    value: [FangraphTeam.Braves],
+    label: 'Braves',
+  },
+  {
+    value: [FangraphTeam.Brewers],
+    label: 'Brewers',
+  },
+  {
+    value: [FangraphTeam.Cardinals],
+    label: 'Cardinals',
+  },
+  {
+    value: [FangraphTeam.Cubs],
+    label: 'Cubs',
+  },
+  {
+    value: [FangraphTeam.Diamondbacks],
+    label: 'Diamondbacks',
+  },
+  {
+    value: [FangraphTeam.Dodgers],
+    label: 'Dodgers',
+  },
+  {
+    value: [FangraphTeam.Giants],
+    label: 'Giants',
+  },
+  {
+    value: [FangraphTeam.Guardians],
+    label: 'Guardians',
+  },
+  {
+    value: [FangraphTeam.Mariners],
+    label: 'Mariners',
+  },
+  {
+    value: [FangraphTeam.Marlins],
+    label: 'Marlins',
+  },
+  {
+    value: [FangraphTeam.Mets],
+    label: 'Mets',
+  },
+  {
+    value: [FangraphTeam.Nationals],
+    label: 'Nationals',
+  },
+  {
+    value: [FangraphTeam.Orioles],
+    label: 'Orioles',
+  },
+  {
+    value: [FangraphTeam.Padres],
+    label: 'Padres',
+  },
+  {
+    value: [FangraphTeam.Phillies],
+    label: 'Phillies',
+  },
+  {
+    value: [FangraphTeam.Pirates],
+    label: 'Pirates',
+  },
+  {
+    value: [FangraphTeam.Rangers],
+    label: 'Rangers',
+  },
+  {
+    value: [FangraphTeam.Rays],
+    label: 'Rays',
+  },
+  {
+    value: [FangraphTeam.RedSox],
+    label: 'Red Sox',
+  },
+  {
+    value: [FangraphTeam.Reds],
+    label: 'Reds',
+  },
+  {
+    value: [FangraphTeam.Rockies],
+    label: 'Rockies',
+  },
+  {
+    value: [FangraphTeam.Royals],
+    label: 'Royals',
+  },
+  {
+    value: [FangraphTeam.Tigers],
+    label: 'Tigers',
+  },
+  {
+    value: [FangraphTeam.Twins],
+    label: 'Twins',
+  },
+  {
+    value: [FangraphTeam.WhiteSox],
+    label: 'White Sox',
+  },
+  {
+    value: [FangraphTeam.Yankees],
+    label: 'Yankees',
+  },
+];
