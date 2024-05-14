224126c72edc17c315603159590ddb88cb7dff95
Nathan Pinciak <nathan.pinciak@outlook.com>
2024-05-14 15:15:32 -0400

    Add back starting lineups

--- src/features/espn/fantasy-baseball/helpers/index.ts
+++ src/features/espn/fantasy-baseball/helpers/index.ts
@@ -1,2 +1,3 @@
+export * from './baseball-helpers';
 export * from './baseball-scoring-period';
 export * from './mlb-advanced-stats';
--- src/features/espn/fantasy-baseball/pages/baseball-team/baseball-team.page.tsx
+++ src/features/espn/fantasy-baseball/pages/baseball-team/baseball-team.page.tsx
@@ -1,10 +1,12 @@
 import { TypeSortInfo } from '@inovua/reactdatagrid-community/types';
 import { useParams } from 'react-router-dom';
+import { BASEBALL_LINEUP_MAP } from 'sports-ui-sdk';
 import {
   useCreateEspnPlayerMutation,
   useGetFangraphProjectionsQuery,
 } from '../../../../../@shared/supabase/supabase.client';
 import { useFetchTeamByIdQuery } from '../../client/fantasy-baseball.client';
+import { startingPlayersFilter } from '../../helpers';
 
 export function BaseballTeam() {
   const { year, leagueId, teamId } = useParams<{
@@ -24,7 +26,6 @@ export function BaseballTeam() {
   const defaultSortInfo: TypeSortInfo = [];
 
   const { data: fangraphsProj } = useGetFangraphProjectionsQuery({});
-
 
   // const startingBatters = useSelector(selectTeamStartingLineupBatters)(teamId!);
   // const startingPitchers = useSelector(selectTeamStartingLineupPitchers)(
@@ -83,96 +84,110 @@ export function BaseballTeam() {
     // await createEspnPlayer(player);
   };
 
+  const batters = team?.roster?.filter(
+    p => !p.isPitcher || p.lineupSlotId === 12
+  );
+
+  const pitchers = team?.roster?.filter(
+    p => p.isPitcher && p.lineupSlotId !== 12
+  );
+
+  const startingBatters = startingPlayersFilter(
+    batters ?? [],
+    BASEBALL_LINEUP_MAP
+  );
+
+  const startingPitchers = startingPlayersFilter(
+    pitchers ?? [],
+    BASEBALL_LINEUP_MAP
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
-    <>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div key={team?.id}>
-          <div className="grid grid-cols-3 text-left mb-5 mt-5">
-            <div>
-              <img
-                alt={team?.name}
-                role="presentation"
-                aria-roledescription="presentation"
-                src={team?.logo}
-              />
-            </div>
-            <div className="col-span-2">
-              <h1>
-                {team?.name} {team?.totalPoints}
-              </h1>
-              <div>
-                <span className="text-xs">Rank: {team?.currentRank}</span>
-              </div>
-            </div>
+    <div key={team?.id}>
+      <div className="grid grid-cols-3 text-left mb-5 mt-5">
+        <div>
+          <img
+            alt={team?.name}
+            role="presentation"
+            aria-roledescription="presentation"
+            src={team?.logo}
+          />
+        </div>
+        <div className="col-span-2">
+          <h1>
+            {team?.name} {team?.totalPoints}
+          </h1>
+          <div>
+            <span className="text-xs">Rank: {team?.currentRank}</span>
           </div>
+        </div>
+      </div>
 
-          <div className="flex text-left">
-            <div className="w-full px-4 xl:w-4/12">
-              {/* {startingBatters.map(player => (
-                <div className="grid grid-cols-4 py-3" key={player.id}>
-                  <div>
-                    <img
-                      className="w-13 h-9 rounded-full text-center text-xs text-gray-500"
-                      src={player.img}
-                      alt={player.name}
-                      role="presentation"
-                      aria-roledescription="presentation"
-                    />
-                  </div>
-                  <div className="col-span-3">
-                    {player.name}
-                    <div className="text-xs">
-                      {player.team}
-                      <span className="font-bold"> {player.lineupSlot}</span>
-                    </div>
-                  </div>
-                </div>
-              ))} */}
-
-              <div className="py-3"></div>
-              {/* {startingPitchers.map(player => (
-                <div className="grid grid-cols-4 py-3" key={player.id}>
-                  <div>
-                    <img
-                      className="w-13 h-9 rounded-full text-center text-xs text-gray-500"
-                      src={player.img}
-                      alt={player.name}
-                      role="presentation"
-                      aria-roledescription="presentation"
-                    />
-                  </div>
-                  <div className="col-span-3">
-                    {player.name}
-                    <div className="text-xs">
-                      {player.team}
-                      <span className="font-bold"> {player.lineupSlot}</span>
-                    </div>
-                  </div>
-                </div>
-              ))} */}
-            </div>
-
-            <div className="w-full px-4 xl:w-8/12">
+      <div className="flex text-left">
+        <div className="w-full px-4 xl:w-4/12">
+          {startingBatters.map(player => (
+            <div className="grid grid-cols-4 py-3" key={player.id}>
               <div>
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={handleSyncBatters}
-                >
-                  Sync Batters
-                </button>
+                <img
+                  className="w-13 h-9 rounded-full text-center text-xs text-gray-500"
+                  src={player.img}
+                  alt={player.name}
+                  role="presentation"
+                  aria-roledescription="presentation"
+                />
               </div>
-              {/* <div>
+              <div className="col-span-3">
+                {player.name}
+                <div className="text-xs">
+                  {player.team}
+                  <span className="font-bold"> {player.lineupSlot}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div className="py-3"></div>
+          {startingPitchers.map(player => (
+            <div className="grid grid-cols-4 py-3" key={player.id}>
+              <div>
+                <img
+                  className="w-13 h-9 rounded-full text-center text-xs text-gray-500"
+                  src={player.img}
+                  alt={player.name}
+                  role="presentation"
+                  aria-roledescription="presentation"
+                />
+              </div>
+              <div className="col-span-3">
+                {player.name}
+                <div className="text-xs">
+                  {player.team}
+                  <span className="font-bold"> {player.lineupSlot}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="w-full px-4 xl:w-8/12">
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleSyncBatters}
+            >
+              Sync Batters
+            </button>
+          </div>
+          {/* <div>
             <BaseballPlayerStatsTable
               data={playerStats}
               defaultSortInfo={defaultSortInfo}
             />
           </div> */}
-            </div>
-          </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
