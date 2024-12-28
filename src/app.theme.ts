bc8c8bc99b0308ff15d821efeca7c6c63160cc5d
Nathan Pinciak <natepinciak@live.com>
2024-12-28 17:15:24 -0500

    Add dark mode

--- src/app.theme.ts
+++ src/app.theme.ts
@@ -1,32 +1,11 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { createTheme } from '@mui/material/styles';
 
 export const AppTheme = createTheme({
   palette: {
-    primary: {
-      main: '#3b82f6',
-    },
-    secondary: {
-      main: '#1e293b',
-    },
-  },
-  components: {
-    // Name of the component
-    MuiAppBar: {
-      styleOverrides: {
-        root: {
-          backgroundColor: '#1e293b',
-          color: '#fff',
-        },
-      },
-    },
-    MuiButton: {
-      styleOverrides: {
-        // Name of the slot
-        root: {
-          // Some CSS
-          fontSize: '1rem',
-        },
-      },
-    },
+    mode: 'dark',
   },
 });
