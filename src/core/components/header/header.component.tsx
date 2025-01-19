bde74db5cf66a0a8b7a39f90736d5b769640c27b
Nathan Pinciak <natepinciak@live.com>
2025-01-19 13:58:30 -0500

    Add bottom navigation to make it feel more of an app

--- src/core/components/header/header.component.tsx
+++ src/core/components/header/header.component.tsx
@@ -2,7 +2,6 @@ import AdbIcon from '@mui/icons-material/Adb';
 import MenuIcon from '@mui/icons-material/Menu';
 import { ListItemText, MenuList } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
@@ -10,7 +9,6 @@ import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -155,47 +153,6 @@ export function HeaderComponent() {
                 {route.navigationLabel}
               </Button>
             ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar
-                  alt={profile?.user_name}
-                  src="/static/images/avatar/2.jpg"
-                />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuList>
-                {profileNavigationLinks.map((route, key) => {
-                  return (
-                    <MenuItem
-                      key={key}
-                      onClick={() => handleNavigationClick(route)}
-                      selected={activeRoute(route.path)}
-                    >
-                      <ListItemText primary={route.navigationLabel} />
-                    </MenuItem>
-                  );
-                })}
-              </MenuList>
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
