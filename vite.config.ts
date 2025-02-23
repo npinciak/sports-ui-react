49d5e0341c75b0da67888fce9084036a250035e9
Nathan Pinciak <natepinciak@live.com>
2025-02-23 12:01:49 -0500

    Update path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -7,4 +8,10 @@ export default defineConfig({
     include: ['@mui/material', '@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
   },
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './src/@shared'),
+    },
+  },
 });
