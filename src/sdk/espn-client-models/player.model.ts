938a0e1230d733488ae2f6dcbc10a3c54b6b0dc3
Nathan Pinciak <natepinciak@live.com>
2025-03-21 20:00:48 -0400

    Add `mlb` position map

--- src/sdk/espn-client-models/player.model.ts
+++ src/sdk/espn-client-models/player.model.ts
@@ -1,7 +1,25 @@
 import { PlayerCompetitionStatus } from '../injury/injury-status.model';
+import { ClientBaseballLineupSlot } from './baseball/lineup';
+import { MlbPosition } from './baseball/position/mlb-position.model';
+import { ClientMlbTeam } from './baseball/team/mlb-team.model';
 import { IClientPlayerInjustStatusByGameMap } from './game-status.model';
 import { IClientPlayerRatingsMapByTimePeriod } from './player-rating.model';
 import { IClientPlayerStatsYearEntity } from './player-stats.model';
+
+export interface IClientSimplePlayerEntity {
+  defaultPositionId: MlbPosition;
+  droppable: boolean;
+  eligibleSlots?: ClientBaseballLineupSlot[];
+  firstName: string;
+  fullName: string;
+  id: number;
+  lastName: string;
+  ownership?: {
+    percentOwned: number;
+  };
+  proTeamId: ClientMlbTeam;
+  universeId: number;
+}
 
 export interface IClientPlayerEntity {
   id: number;
