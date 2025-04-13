6eab7c36bd17386cbd6d3aff1ff78769c85bc9bc
Nathan Pinciak <natepinciak@live.com>
2025-04-13 13:49:59 -0400

    Update `SelectComponent` default value

--- src/@shared/components/select.component.tsx
+++ src/@shared/components/select.component.tsx
@@ -1,21 +1,25 @@
 import { FormControl, InputLabel, NativeSelect } from '@mui/material';
 
+interface SelectComponentProps {
+  label?: string;
+  options: { value: string | number; label: string }[];
+  defaultValue?: string | number;
+  onHandleOptionChange: (value: string) => void;
+}
+
 export function SelectComponent({
   label,
   options,
+  defaultValue,
   onHandleOptionChange,
-}: {
-  label: string;
-  options: { value: string | number; label: string }[];
-  onHandleOptionChange: (value: string) => void;
-}) {
+}: SelectComponentProps) {
   return (
     <FormControl fullWidth>
       <InputLabel variant="standard" htmlFor="uncontrolled-native">
         {label}
       </InputLabel>
       <NativeSelect
-        defaultValue={30}
+        defaultValue={defaultValue}
         inputProps={{
           name: label,
           id: 'uncontrolled-native',
