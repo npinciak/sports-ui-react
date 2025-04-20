122917ffd32cc6d7e97d0b09296b08558af39495
Nathan Pinciak <natepinciak@live.com>
2025-04-20 10:11:17 -0400

    Remove chance to win in fastcast

--- src/features/espn/fastcast/components/fastcast-event.component.tsx
+++ src/features/espn/fastcast/components/fastcast-event.component.tsx
@@ -2,8 +2,6 @@ import { Card, CardContent, Divider } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import { FastcastEvent } from '../models/fastcast-event.model';
 import { fastcastEventSummary } from '../transformers/espn-fastcast.transformers';
-import { FastcastChanceToWinAwayComponent } from './fastcast-event-chance-to-win/fastcast-event-chance-to-win-away.component';
-import { FastcastChanceToWinHomeComponent } from './fastcast-event-chance-to-win/fastcast-event-chance-to-win-home.component';
 import { FastcastTeamComponent } from './fastcast-team.component';
 
 interface FastcastEventComponentProps {
@@ -28,34 +26,12 @@ export function FastcastEventComponent({ event }: FastcastEventComponentProps) {
               <FastcastTeamComponent team={event.teams?.home} />
             </Grid>
             {event.state === 'in' && (
-              <>
-                <Grid
-                  container
-                  className="w-full mb-2 text-xs"
-                  columnSpacing={1}
-                >
-                  <Grid size={12}>
-                    <div className="flex w-full">
-                      <FastcastChanceToWinAwayComponent
-                        team={event.teams?.away}
-                      />
-                      <FastcastChanceToWinHomeComponent
-                        team={event.teams?.home}
-                      />
-                    </div>
-                  </Grid>
+              <Grid container className="w-full mb-2 text-xs" columnSpacing={1}>
+                <Grid size={12}>
+                  {event.lastPlay?.text}
+                  {event.drive?.description}
                 </Grid>
-                <Grid
-                  container
-                  className="w-full mb-2 text-xs"
-                  columnSpacing={1}
-                >
-                  <Grid size={12}>
-                    {event.lastPlay?.text}
-                    {event.drive?.description}
-                  </Grid>
-                </Grid>
-              </>
+              </Grid>
             )}
           </CardContent>
         </Card>
