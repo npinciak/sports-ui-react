05ed60478127b99b68bc0c4ebf5709ae3d5de04b
Nathan Pinciak <natepinciak@live.com>
2025-04-24 18:35:54 -0400

    Add PWA support with vite-plugin-pwa
    - Configured PWA settings including manifest and icons.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,58 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   optimizeDeps: {
     include: ['@mui/material', '@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'SportsSaga',
+        short_name: 'SportsSaga',
+        description: 'SportsSaga is a fantasy sports app that provides real-time data and insights for fantasy sports enthusiasts.',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'favicon',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'favicon',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon',
+          },
+          {
+            src: '/maskable_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#171717',
+        background_color: '#f0e7db',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@shared': path.resolve(__dirname, './src/@shared'),
